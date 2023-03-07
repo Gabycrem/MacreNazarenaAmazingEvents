87acfba1725a0f7777cade3b98015894c16e3af3
gabycrem <gabymacre@gmail.com>
2023-03-07 19:38:21 -0300

    card Details

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,39 +1,5 @@
-
-let cards = document.getElementById('cards');
-let fragmentCards = document.createDocumentFragment();
-
-for (let events of data.events) {
-    let card = document.createElement('div');
-    card.classList.add('card');
-    card.innerHTML = `
-    <img src="${events.image}" class="card-img-top" alt="...">
-    <div id="card-center">
-        <h5 class="card-title">${events.name}</h5>
-        <p class="card-text">${events.description}</p>
-    </div>
-    <div class="d-flex justify-content-evenly" id="card-footer">
-        <p class="d-inline-block card-text">Price: $${events.price}</p>
-        <a href="./details.html" class="btn btn-dark color-text card-link">Details</a>
-    </div>`;
-    fragmentCards.appendChild(card);
-}
-
-cards.appendChild(fragmentCards);
-// Plantilla de Tarjetas.
-// <div class="card">
-//     <img src="./assets/img/cinema.jpg" class="card-img-top" alt="...">
-//         <div class="card-body">
-//             <h5 class="card-title">Card title</h5>
-//             <p class="card-text">Some quick example text....</p>
-//         </div>
-//         <div class="card-body d-flex justify-content-evenly">
-//             <p class="d-inline-block card-text">Price: $1111</p>
-//             <a href="./details.html" class="btn btn-dark color-text card-link">Another link</a>
-//         </div>
-// </div> 
-
-//Carga de categorias dinámica
-
+// CARGA DE CATEGORÍAS - 
+//TRAIGO CATEGORÍAS Y ELIMINO REPETIDAS
 let checkbox = document.getElementById('check');
 let fragmentCheck = document.createDocumentFragment();
 let categories = [];
@@ -49,23 +15,71 @@ const catNotDups = (unArray) => {
         return array
     }, []);
 }
-categories = (catNotDups(categories));
+categories = catNotDups(categories);
 
-let cont = 0;
+//CARGO CATEGORÍAS SIN REPETIR Y CREO ELEMENTOS HTML
+
 for (let category of categories) {
     let check = document.createElement('div');
-    cont++;
     check.classList.add('check-inline');
-    check.innerHTML = `<input class="form-check-input" type="checkbox" name="inlineRadioOptions" id="inlineRadio${cont}"
+    check.innerHTML = `<input class="form-check-input" type="checkbox" name="inlineRadioOptions" id="${category.replace(' ', '-').toLowerCase()}"
              value="${category}">
-             <label class="form-check-label" for="inlineRadio${cont}">${category}</label>`;
+             <label class="form-check-label" for="${category.replace(' ', '-').toLowerCase()}">${category}</label>`;
     fragmentCheck.appendChild(check);
 }
 checkbox.appendChild(fragmentCheck);
 
-// Plantilla de checkbox
-// {/* <div class="check-inline">
-//     <input class="form-check-input" type="checkbox" name="inlineRadioOptions" id="inlineRadio2"
-//         value="Book Exchange">
-//         <label class="form-check-label" for="inlineRadio2">Book Exchange</label>
-// </div> */}
+
+//-------------------------------
+// TRAER TODOS LOS CHECKBOX
+
+let checkboxes = document.querySelectorAll('input[type=checkbox]');
+
+console.log(checkboxes);
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', seleccionados);
+})
+// FILTRAR LOS SELECCIONADOS
+function seleccionados() {
+    let chequeados = Array.from(checkboxes).filter(checkbox => checkbox.checked);
+    console.log(chequeados);
+    let inputsValue = chequeados.map(input => input.value);
+    console.log(inputsValue);
+    let eventFiltr = data.events.filter(event => inputsValue.includes(event.category));
+    console.log(eventFiltr)
+    
+}
+
+
+// IMPRIMIR FILTRADOS
+
+
+// CARGA DE TARJETAS DINÁMICAS
+cargarCard();
+
+function cargarCard() {
+    let cards = document.getElementById('cards');
+    let fragmentCards = document.createDocumentFragment();
+
+    for (let events of data.events) {
+
+        let card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+    <img src="${events.image}" class="card-img-top" alt="...">
+    <div id="card-center">
+        <h5 class="card-title">${events.name}</h5>
+        <p class="card-text">${events.description}</p>
+    </div>
+    <div class="d-flex justify-content-evenly" id="card-footer">
+        <p class="d-inline-block card-text">Price: $${events.price}</p>
+        <a href="./details.html?id=${events._id}" class="btn btn-dark color-text card-link">Details</a>
+    </div>`;
+        fragmentCards.appendChild(card);
+    }
+
+    cards.appendChild(fragmentCards);
+}
+
+//-------------------------------
+

ff02a7eb40e541bebfe923824444d74ff3747ae1
gabycrem <gabymacre@gmail.com>
2023-03-09 17:15:51 -0300

    corrigiendo A

---
+++ assetss/js/details.js
@@ -0,0 +1,42 @@
+const queryString = location.search;
+console.log(queryString)
+const params = new URLSearchParams(queryString);
+console.log([params]);
+const id = params.get("id");
+console.log(id);
+
+const evento = data.events.find(elemento => elemento._id == id);
+
+console.table(evento);
+let claves = (Object.keys(evento));
+
+function cargarCard(){
+    const detailsCard = document.getElementById('details-card');
+    detailsCard.innerHTML = `<div class="w-50">
+    <img src="${evento.image}" class="img-details" alt="...">
+  </div>
+  <div class="w-50">
+    <div class="card-body">
+      <h5 class="card-title d-inline">${claves[2].charAt(0).toUpperCase().concat(claves[2].slice(1))}: ${evento.name}</h5>
+      <p class="">${claves[3].charAt(0).toUpperCase().concat(claves[3].slice(1))}: ${evento.date}</p>
+      <p class="card-text">${claves[4].charAt(0).toUpperCase().concat(claves[4].slice(1))}: ${evento.description}</p>
+      <p class="card-text">${claves[5].charAt(0).toUpperCase().concat(claves[5].slice(1))}: ${evento.category}</p>
+      <p class="card-text">${claves[6].charAt(0).toUpperCase().concat(claves[6].slice(1))}: ${evento.place}</p>
+      <p class="card-text">${claves[7].charAt(0).toUpperCase().concat(claves[7].slice(1))}: ${evento.capacity}</p>
+      <p class="card-text">${claves[8].charAt(0).toUpperCase().concat(claves[8].slice(1))}: ${evento.assistance? evento.assistance : evento.estimate}</p>
+      <p class="card-text">${claves[9].charAt(0).toUpperCase().concat(claves[9].slice(1))}: $${evento.price}.-</p>
+      
+      <button class="btn btn-dark color-text card-link onclick="back();">Volver atrás</button>
+    </div>
+  </div>`
+
+}
+function back(){
+    window.history.back();
+}
+cargarCard();
+
+
+
+
+
---
+++ assetss/js/uncoming-events.js
@@ -0,0 +1,104 @@
+// CARGA DINAMICA CHECKBOX
+
+let checkbox = document.getElementById('check');
+let categoriesNotDup = [];
+let categories = data.events.map(event => {
+    if (!categoriesNotDup.includes(event.category)){
+        categoriesNotDup.push(event.category);
+    }
+});
+
+console.log(categoriesNotDup);
+
+let fragmentCheck = document.createDocumentFragment();
+let cont = 0;
+for (let category of categoriesNotDup) {
+    let check = document.createElement('div');
+    cont++;
+    check.classList.add('check-inline');
+    check.innerHTML = `<input class="form-check-input" type="checkbox" name="inlineRadioOptions" id="inlineRadio${cont}"
+             value="${category}">
+             <label class="form-check-label" for="inlineRadio${cont}">${category}</label>`;
+    fragmentCheck.appendChild(check);
+}
+checkbox.appendChild(fragmentCheck);
+
+let chequeados = [];
+let searchFilter = [];
+
+let checkboxes = document.querySelectorAll('input[type=checkbox]');
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', () => {
+        chequeados = Array.from(checkboxes).filter(checkbox => checkbox.checked).map(input => input.value);
+        cargarCardPast(filtrarTodo(data.events), 'cards');
+    })
+});
+
+let inputSearch = document.getElementById('inputSearchUncoming');
+inputSearch.addEventListener('keyup', () => {
+    cargarCardPast(filtrarTodo(data.events), 'cards');
+})
+
+function filtrarPorBuscado(value, miObjeto) {
+    if (value == '') return miObjeto;
+    return miObjeto.filter(elemento => elemento.name.toLowerCase().includes(value.toLowerCase().trim()));
+}
+
+function filtrarPorCheck(arrayStr, arrayObj) {
+    if (arrayStr.length == 0) return arrayObj;
+    return arrayObj.filter(event => arrayStr.includes(event.category));
+}
+
+function filtrarTodo(array) {
+    let checkFilter = filtrarPorCheck(chequeados, array);
+    searchFilter = filtrarPorBuscado(inputSearch.value, checkFilter);
+    console.log(searchFilter);
+    return searchFilter;
+}
+
+
+cargarCardPast(data.events, 'cards');
+//CARGA DE TARJETAS
+
+function cargarCardPast(miObjeto, unId) {
+    let currentDateString = data.currentDate;
+    let currentDate = new Date(currentDateString);
+
+    let cards = document.getElementById(unId);
+    let fragment = document.createDocumentFragment();
+    cards.innerHTML = '';
+    
+    for (let events of miObjeto) {
+
+        let eventeDateString = events.date;
+        let eventDate = new Date(eventeDateString);
+
+        if (eventDate > currentDate) {
+            if (searchFilter.length == 0 && inputSearch.value != '') {
+                let card = document.createElement('div');
+                card.classList.add('card' ,'my-card', 'mb-3','d-flex', 'flex-row', 'w-50', 'p-1', 'gap-2');
+                card.innerHTML = `<div id="card-center">
+                <h5 class="card-title">NO EXISTE</h5>
+                <p class="card-text">Su búsqueda no coincide con ningún evento</p>
+            </div>`;
+                cards.appendChild(card);
+            }
+            let card = document.createElement('div');
+            card.classList.add('card');
+            card.innerHTML = `
+        <img src="${events.image}" class="card-img-top" alt="...">
+        <div id="card-center">
+            <h5 class="card-title">${events.name}</h5>
+            <p class="card-text">${events.description}</p>
+        </div>
+        <div class="d-flex justify-content-evenly" id="card-footer">
+            <p class="d-inline-block card-text">Price: $${events.price}</p>
+            <a href="./details.html?id=${events._id}" class="btn btn-dark color-text card-link">Details</a>
+        </div>`;
+            fragment.appendChild(card);
+        }
+    }
+
+    cards.appendChild(fragment);
+}
+

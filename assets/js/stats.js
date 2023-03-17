3fcc7d21e23cf1c54a6d7600e76d144a57c23f9f
gabycrem <gabymacre@gmail.com>
2023-03-17 17:44:06 -0300

    cargando tabla 1

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,4 +1,12 @@
 let urlApi = "https://mindhub-xj03.onrender.com/api/amazing"
+let eventsPast = [];
+let eventsUncoming = [];
+let revenuesCat = [];
+let tabla1 = document.getElementById('tabla1');
+let tabla2 = document.getElementById('tabla2');
+let tabla3 = document.getElementById('tabla3');
+let tablaPast = [];
+let tablaUncoming = [];
 
 
 traerDatos()
@@ -6,13 +14,19 @@ async function traerDatos() {
     try {
         const response = await fetch(urlApi)
         const datos = await response.json();
-        pastEvents(datos.events , datos)
-    }
-    catch(error){
-        console.error('ERRORRRRRRR'+error);
-    }    
-}
+        pastEvents(datos.events, datos);
+        //console.log(eventsPast);
+        porcentajeAsistencia(eventsPast);
+        tablaPast.forEach(cat => { ganancias(eventsPast, cat)});
+        tablaUncoming.forEach(cat => { ganancias(eventsUncoming, cat)});
+        
+        //categoriesFilt(datos.events);
 
+    }
+    catch (error) {
+        console.error('ERRORRRRRRR' + error);
+    }
+}
 
 function currentDate(miObjeto) {
     let currentDateString = miObjeto.currentDate;
@@ -27,16 +41,114 @@ function pastEvents(miObjeto, miJson) {
         let eventDate = new Date(eventeDateString);
 
         if (eventDate < currentDate(miJson)) {
-            console.log(evento.date+' evento pasado');
-            
-            //acá tengo que llamar las funciones matemáticas...
-        }else{
-            console.log(`${evento.date} evento futuro`)
+            eventsPast.push(evento);
+
+        } else {
+            eventsUncoming.push(evento);
         }
     }
+    categoriesFilt(eventsPast);
+    categoriesFilt(eventsUncoming);
 }
 
-// let porcentajesAsistance
-// function porcentajeDeAsist(unEvento) {
-//     unEvento.capacity
-// }
+
+function categoriesFilt(miObjeto) {
+    let categories = [];
+    miObjeto.map(event => {
+        if (!categories.includes(event.category)) {
+            categories.push(event.category);
+             
+        }
+
+    })
+    console.log(miObjeto)
+    if (miObjeto == 'eventsPast'){
+        tablaPast = categories;
+        //console.log(tablaPast)
+    } else {
+        tablaUncoming = categories;
+        console.log(tablaUncoming)
+    }
+
+}
+
+function ganancias(unArray, categoria) {
+    //console.log(categorias)
+    let revenues = 0;
+    let sumaAss = 0;
+    let contCat = 0;
+    for (evento of unArray) {
+        if (evento.category == categoria) {
+            contCat++
+            revenues += (evento.assistance ? evento.assistance : evento.estimate) * evento.price;
+            sumaAss += parseFloat(((evento.assistance ? evento.assistance : evento.estimate) * 100 / evento.capacity).toFixed(2));
+        }
+        if (evento.assistance && !tablaPast.includes(categoria)) {
+            tablaPast.push(categoria);
+        } else if (evento.estimate && !tablaUncoming.includes(categoria)) {
+            tablaUncoming.push(categoria);
+        }
+    }
+    sumaAss = parseFloat(sumaAss / contCat).toFixed(2);
+    console.log(categoria + ' ' + sumaAss + '% -- Revenues: $' + revenues)
+
+
+}
+
+
+
+function cargarTabla2y3(tabla, col1, col2, col3) {
+    tabla.innerHTML = `
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>`
+}
+
+function cargarTabla1(miObjeto) {
+    tabla1.innerHTML = `
+    <tr>
+        <td> ${miObjeto.eventMayPorAsist} </td>
+        <td> ${miObjeto.eventMenPorAsist}</td>
+        <td> ${miObjeto.eventMayCapacity} </td>
+    </tr>`
+}
+function porcentajeAsistencia(unArray) {
+    let contTabla1 = {};
+    let eventMayPorAsist = '';
+    let eventMenPorAsist = '';
+    let eventMayCapacity = '';
+    let porcentajeMay = 0;
+    let porcentajeMen = 100;
+    let capacity = 0;
+    for (evento of unArray) {
+        let auxPorcentaje = ((evento.assistance * 100) / evento.capacity).toFixed(2);
+        if (auxPorcentaje > porcentajeMay) {
+            porcentajeMay = auxPorcentaje;
+            eventMayPorAsist = evento.name;
+        } else if (auxPorcentaje < porcentajeMen) {
+            porcentajeMen = auxPorcentaje;
+            eventMenPorAsist = evento.name;
+        }
+        let auxCapacity = evento.capacity;
+        if (auxCapacity > capacity) {
+            capacity = auxCapacity;
+            eventMayCapacity = evento.name;
+        }
+    }
+    contTabla1.eventMayPorAsist = `${eventMayPorAsist}:  ${porcentajeMay}%`;
+    contTabla1.eventMenPorAsist = `${eventMenPorAsist}:  ${porcentajeMen}%`;
+    contTabla1.eventMayCapacity = `${eventMayCapacity}:  ${capacity}`;
+    cargarTabla1(contTabla1);
+}
